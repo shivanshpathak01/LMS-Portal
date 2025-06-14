7345b8480590e14159edc2cf175272f23b641b0c
Shivansh Pathak <shivanshpathak0001@gmail.com>
2025-06-14 10:01:01 +0530

    ++

--- client/src/components/student/Companies.jsx
+++ client/src/components/student/Companies.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import microsoft_logo from '../../assets/microsoft_logo.svg'
+import accenture_logo from '../../assets/accenture_logo.svg'
+import walmart_logo from '../../assets/walmart_logo.svg'
+import adobe_logo from '../../assets/adobe_logo.svg'
+import paypal_logo from '../../assets/paypal_logo.svg'
 
 function Companies() {
   return (
-    <div>Companies</div>
+    <div className='pt-16'>
+      <p className='text-base text-gray-500'>Trusted by Learners</p>
+      <div className='flex flex-wrap items-center justify-center gap-6 md:gap-16 md:mt-10 mt-5'> 
+        <img src={microsoft_logo} alt="Microsoft" className='w-20 md:28' />
+        <img src={accenture_logo} alt="Accenture" className='w-20 md:28' />
+        <img src={walmart_logo} alt="Walmart" className='w-20 md:28' />
+        <img src={adobe_logo} alt="Adobe" className='w-20 md:28' />
+        <img src={paypal_logo} alt="Paypal" className='w-20 md:28' />
+      </div>
+      </div>
   )
 }
 
---
+++ client/src/components/student/CourseCard.jsx
@@ -0,0 +1,29 @@
+import React, { useContext } from 'react'
+import { assets } from '../../assets/assets'
+import { AppContext } from '../../context/AppContext'
+
+function CourseCard({course}) {
+// 2:02:00
+    const {currency} = useContext(AppContext)
+
+  return (
+    <div>
+        <img src="{course.courseThumbnail}" alt="" />
+        <div>
+            <h3>{course.courseTitle}</h3>
+            <p>{course.educator.name}</p>
+            <div>
+                <p>4.5</p>
+                <div>
+                    {[...Array(5)].map((_,i)=>(<img key={i} src={assets.star} alt=''/>
+                    ))}
+                </div>
+                <p>22</p>
+            </div>
+            <p>{(course.coursePrice - course.discount*course.coursePrice / 100).toFixed(2)}</p>
+        </div>
+    </div>
+  )
+}
+
+export default CourseCard
