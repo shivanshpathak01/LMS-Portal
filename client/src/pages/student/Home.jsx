a7859b36302b1db49e9cb65080a00c19e1e86cef
Shivansh Pathak <shivanshpathak0001@gmail.com>
2025-06-16 13:11:27 +0530

    Footer done

--- client/src/pages/student/Home.jsx
+++ client/src/pages/student/Home.jsx
@@ -4,6 +4,7 @@ import Companies from '../../components/student/Companies'
 import CoursesSection from '../../components/student/CoursesSection'
 import TestimonialsSection from '../../components/student/TestimonialsSection'
 import CallToAction from '../../components/student/CallToAction'
+import Footer from '../../components/student/Footer'
 
 function Home() {
   return (
@@ -12,7 +13,8 @@ function Home() {
       <Companies/>
       <CoursesSection/>
       <TestimonialsSection/>
-      <CallToAction/>    
+      <CallToAction/>  
+      <Footer/>  
     </div>
   )
 }
