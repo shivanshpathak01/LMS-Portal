6fefd3a5c83cf18145bb920250038e2a18516357
Shivansh Pathak <shivanshpathak0001@gmail.com>
2025-06-17 20:42:07 +0530

    my enrollment page completed at 5:12:54

--- client/src/pages/student/MyEnrollments.jsx
+++ client/src/pages/student/MyEnrollments.jsx
@@ -1,5 +1,7 @@
 import React, { useContext, useState } from 'react'
 import { AppContext } from '../../context/AppContext'
+import {Line} from 'rc-progress'
+import Footer from '../../components/student/Footer'
 
 function MyEnrollments() {
 
@@ -42,6 +44,9 @@ function MyEnrollments() {
                 <img src={course.courseThumbnail} alt="" className='w-14 sm:w-24 md:w-28' />
                 <div className='flex-1'>
                   <p className='mb-1 max-sm:text-sm'>{course.courseTitle}</p>
+                  <Line strokeWidth={2} percent={
+                    progressArray[index] ? (progressArray[index].lectureCompleted * 100) / progressArray[index].totalLectures : 0
+                  } className='bg-gray-300 rounded-full'/>
                 </div>
               </td>
               <td className='px-4 py-3 max-sm:hidden'>
@@ -61,6 +66,7 @@ function MyEnrollments() {
         </tbody>
       </table>
     </div>
+    <Footer/>
     </>
   )
 }
