2195ed78b95ab70e66f1c2c138aa51abea0969e5
Shivansh Pathak <shivanshpathak0001@gmail.com>
2025-06-18 15:19:34 +0530

    educator Dashboard almost done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -31,8 +31,8 @@ function App() {
                 <Route path='/player/:courseId' element={<Player />} />
                 <Route path='/loading/:path' element={<Loading />} />
 
-                <Route path='educator' element={<Educator/>}>
-                    <Route path='educator' element={<Dashboard/>}/>
+                <Route path='/educator' element={<Educator/>}>
+                    <Route path='/educator' element={<Dashboard/>}/>
                     <Route path='add-course' element={<AddCourse/>}/>
                     <Route path='my-courses' element={<MyCourses/>}/>
                     <Route path='student-enrolled' element={<StudentsEnrolled/>}/>
