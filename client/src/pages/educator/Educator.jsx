b877c1b44802570c1839fd3175742e3e0db1fc7b
Shivansh Pathak <shivanshpathak0001@gmail.com>
2025-06-18 09:37:38 +0530

    Educator Navbar done

--- client/src/pages/educator/Educator.jsx
+++ client/src/pages/educator/Educator.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Navbar from '../../components/educator/Navbar'
 
 function Educator() {
     return (
-        <div>
-            <h1>Educator</h1>
+        <div className='text-default min-h-screen bg-white'>
+            <Navbar/>
             <div>
                 <Outlet />
             </div>
