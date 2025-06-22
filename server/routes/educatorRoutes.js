b859a73952896c6d72ac3c50635668441e9c5011
Shivansh Pathak <shivanshpathak0001@gmail.com>
2025-06-22 17:13:13 +0530

    Educator routes almost done

--- server/routes/educatorRoutes.js
+++ server/routes/educatorRoutes.js
@@ -1,9 +1,17 @@
 import express from 'express'
-import { updateRoleToEducator } from '../controllers/educatorControllers.js'
+import { addCourse, educatorDashboardData, getEducatorCourses, getEnrolledStudentsData, updateRoleToEducator } from '../controllers/educatorControllers.js'
+import upload from '../configs/multer.js'
+import { protectEducator } from '../middlewares/authMiddleware.js'
 
 const educatorRouter = express.Router()
 
 // Add educator role
 educatorRouter.get('/update-role', updateRoleToEducator)
+educatorRouter.post('/add-course', upload.single('image'),protectEducator,addCourse)
+educatorRouter.get('/courses',protectEducator,getEducatorCourses)
+educatorRouter.get('/dashboard',protectEducator,educatorDashboardData)
+educatorRouter.get('/enrolled-students',protectEducator,getEnrolledStudentsData)
+
+
 
 export default educatorRouter
