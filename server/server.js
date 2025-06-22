487788e9ff56983a2bbcd615b6e2eb39934e4b9d
Shivansh Pathak <shivanshpathak0001@gmail.com>
2025-06-22 20:56:16 +0530

    mostly all APIs done and working perfectly

--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ import { clerkwebhooks } from './controllers/webhooks.js'
 import educatorRouter from './routes/educatorRoutes.js'
 import { clerkMiddleware } from '@clerk/express'
 import connectCloudinary from './configs/cloudinary.js'
+import courseRouter from './routes/courseRoute.js'
+import userRouter from './routes/userRoutes.js'
 
 const app = express()
 
@@ -21,6 +23,8 @@ app.use(clerkMiddleware())
 app.get('/',(req,res)=>res.send("API working!!!"))
 app.post('/clerk', express.json(), clerkwebhooks)
 app.use('/api/educator', express.json(), educatorRouter)
+app.use('/api/course',express.json(),courseRouter)
+app.use('/api/user',express.json(),userRouter)
 
 //Port 
 const PORT = process.env.PORT || 5000
