7b0ec5e3eb112d856e58766aa2e934943434b86e
Shivansh Pathak <shivanshpathak0001@gmail.com>
2025-06-23 10:22:37 +0530

    Usercontroller updated with stripe payment method

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,8 @@
 import User from "../models/User.js"
+import Course from "../models/Course.js"
+import Purchase from "../models/Purchase.js"
+import Stripe from "stripe"
+
 
 // Get user data
 export const getUserData = async (req,res)=>{
@@ -24,4 +28,60 @@ export const userEnrolledCourses = async(req,res)=>{
     } catch (error) {
         res.json({success:false, message:error.message})
     }
+}
+
+// Purchase Course
+export const purchaseCourse = async (req, res) => {
+    try {
+        const { courseId } = req.body;
+        const {origin} = req.headers
+        const userId = req.auth.userId;
+        const userData = await User.findById(userId)
+        const courseData = await Course.findById(courseId);
+
+        if (!userData||!courseData) {
+          return res.json({ success: false, message: "Data not found" });
+        }
+    
+        const purchaseData = {
+          courseId : courseData._id,
+          userId,
+          amount: (courseData.coursePrice - courseData.discount * courseData.coursePrice / 100).toFixed(2),
+          // status: 'pending',
+          // amount: courseData.coursePrice,
+        };
+    
+        const newPurchase = await Purchase.create(purchaseData);
+
+        // Stripe Gateway initialization
+        const stripeInstance = new Stripe(process.env.STRIPE_SECRET_KEY);
+        const currency = process.env.CURRENCY.toLowerCase()
+
+        // Creating Line items  for Stripe
+        const line_items = [{
+            price_data:{
+                currency,
+                product_data:{
+                    name:courseData.courseTitle
+                },
+                unit_amount: Math.floor(newPurchase.amount)*100
+            },
+            quantity: 1
+        }]
+
+
+        const session = await stripeInstance.checkout.sessions.create({
+            success_url:`${origin}/loading/my-enrollments`,
+            cancel_url:`${origin}/`,
+            line_items:line_items,
+            mode:'payment',
+            metadata:{
+                purchaseId:newPurchase._id.toString()
+            }
+        })
+
+        res.json({ success: true, session_url: session.url });
+      } catch (error) {
+        res.json({ success: false, message: error.message });
+      }
 }
