44d3af32e9c82907444ea826e39d9936663a230c
Shivansh Pathak <shivanshpathak0001@gmail.com>
2025-06-23 19:18:28 +0530

    Updated with backend code

--- client/src/pages/educator/MyCourses.jsx
+++ client/src/pages/educator/MyCourses.jsx
@@ -1,20 +1,36 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { AppContext } from '../../context/AppContext'
 import Loading from '../../components/student/Loading'
+import axios from 'axios'
+import { toast } from 'react-toastify'
 
 function MyCourses() {
 
-  const {allcourses, currency} = useContext(AppContext)
+  const { currency, backendUrl, getToken, isEducator } = useContext(AppContext)
 
   const [courses, setCourses] = useState(null)
 
-  const fetchEducatorCourses = async ()=>{
-    setCourses(allcourses)
+  const fetchEducatorCourses = async () => {
+    try {
+      const token = await getToken()
+      const { data } = await axios.get(backendUrl + '/api/educator/courses', {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+
+      data.success && setCourses(data.courses)
+
+    } catch (error) {
+      toast.error(error.message)
+    }
   }
 
-  useEffect(()=>{
-    fetchEducatorCourses()
-  },[])
+  useEffect(() => {
+    if (isEducator) {
+      fetchEducatorCourses()
+    }
+  }, [])
 
   return courses ? (
     <div className='h-screen flex flex-col items-start justify-between md:p-8 md:pb-0 p-4 pt-8 pb-0'>
@@ -23,31 +39,31 @@ function MyCourses() {
         <div className='flex flex-col items-center max-w-4xl w-full overflow-hidden rounded-md bg-white border border-gray-500/20'>
           <table className='md:table-auto table-fixed w-full overflow-hidden'>
             <thead className='text-gray-900 border-b border-gray-500/20 text-sm text-left'>
-            <tr>
-              <th className='px-4 py-3 font-semibold truncate'>All Courses</th>
-              <th className='px-4 py-3 font-semibold truncate'>Earnings</th>
-              <th className='px-4 py-3 font-semibold truncate'>Students</th>
-              <th className='px-4 py-3 font-semibold truncate'>Published On</th>
-            </tr>
+              <tr>
+                <th className='px-4 py-3 font-semibold truncate'>All Courses</th>
+                <th className='px-4 py-3 font-semibold truncate'>Earnings</th>
+                <th className='px-4 py-3 font-semibold truncate'>Students</th>
+                <th className='px-4 py-3 font-semibold truncate'>Published On</th>
+              </tr>
             </thead>
             <tbody className='text-sm text-gray-500'>
-              {courses.map((course)=>(
+              {courses.map((course) => (
                 <tr key={course._id} className='border-b border-gray-500/20'>
                   <td className='md:px-4 pl-2 md:pl-4 py-3 flex items-center space-x-3 truncate'>
-                    <img src={course.courseThumbnail} alt="course image"  className='w-16'/>
+                    <img src={course.courseThumbnail} alt="course image" className='w-16' />
                     <span className='truncate hidden md:block'> {course.courseTitle} </span>
                   </td>
                   <td className='px-4 py-3'>{currency}{Math.floor(course.enrolledStudents.length * (course.coursePrice - course.discount * course.coursePrice / 100))}</td>
                   <td className='px-4 py-3'>{course.enrolledStudents.length}</td>
                   <td className='px-4 py-3'>{new Date(course.createdAt).toLocaleDateString()}</td>
-                  </tr>
+                </tr>
               ))}
             </tbody>
           </table>
         </div>
       </div>
     </div>
-  ) : <Loading/>
+  ) : <Loading />
 }
 
 export default MyCourses
