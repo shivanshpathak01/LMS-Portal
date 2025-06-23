e13023da95fbf95bae387d60a6659384dc4fb384
Shivansh Pathak <shivanshpathak0001@gmail.com>
2025-06-23 19:17:36 +0530

    Updated with backend code

--- client/src/components/student/CourseCard.jsx
+++ client/src/components/student/CourseCard.jsx
@@ -3,30 +3,30 @@ import { assets } from '../../assets/assets'
 import { AppContext } from '../../context/AppContext'
 import { Link } from 'react-router-dom'
 
-function CourseCard({course}) {
-// 2:44:00 
-    const {currency, calculateRating} = useContext(AppContext)
+function CourseCard({ course }) {
 
-  return (
-    <Link to={'/course/' + course._id} onClick={()=>scrollTo(0,0)} 
-    className='border border-gray-500/30 pb-6 overflow-hidden rounded-lg'>
-        <img className='w-full' src={course.courseThumbnail} alt="" />
-        <div className='p-3 text-left'>
-            <h3 className='text-base font-semibold'>{course.courseTitle}</h3>
-            <p className='text-gray-500'>thatbytecoder</p>
-            <div className='flex items-center space-x-2'>
-                <p>{calculateRating(course)}</p>
-                <div className='flex'>
-                    {[...Array(5)].map((_,i)=>(<img key={i} src={i < Math.floor(calculateRating(course)) ? assets.star : assets.star_blank} alt=''
-                    className='w-3.5 h-3.5'/>
-                    ))}
+    const { currency, calculateRating } = useContext(AppContext)
+
+    return (
+        <Link to={'/course/' + course._id} onClick={() => scrollTo(0, 0)}
+            className='border border-gray-500/30 pb-6 overflow-hidden rounded-lg'>
+            <img className='w-full' src={course.courseThumbnail} alt="" />
+            <div className='p-3 text-left'>
+                <h3 className='text-base font-semibold'>{course.courseTitle}</h3>
+                <p className='text-gray-500'>{course.educator?.name || 'Unknown Instructor'}</p>
+                <div className='flex items-center space-x-2'>
+                    <p>{calculateRating(course)}</p>
+                    <div className='flex'>
+                        {[...Array(5)].map((_, i) => (<img key={i} src={i < Math.floor(calculateRating(course)) ? assets.star : assets.star_blank} alt=''
+                            className='w-3.5 h-3.5' />
+                        ))}
+                    </div>
+                    <p className='text-gray-500'>{course.courseRatings.length}</p>
                 </div>
-                <p className='text-gray-500'>{course.courseRatings.length}</p>
+                <p className='text-base font-semibold text-gray-800'>{currency}{(course.coursePrice - course.discount * course.coursePrice / 100).toFixed(2)}</p>
             </div>
-            <p className='text-base font-semibold text-gray-800'>{currency}{(course.coursePrice - course.discount*course.coursePrice / 100).toFixed(2)}</p>
-        </div>
-    </Link>
-  )
+        </Link>
+    )
 }
 
 export default CourseCard
