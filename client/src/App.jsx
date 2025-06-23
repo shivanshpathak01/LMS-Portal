e8640e4f78b873712ddfcc92ecd6726f7f4856fe
Shivansh Pathak <shivanshpathak0001@gmail.com>
2025-06-23 19:17:23 +0530

    Toastify configured

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,6 +13,7 @@ import MyCourses from './pages/educator/MyCourses'
 import StudentsEnrolled from './pages/educator/StudentsEnrolled'
 import Navbar from './components/student/Navbar'
 import "quill/dist/quill.snow.css";
+import { ToastContainer } from 'react-toastify'
 
 
 function App() {
@@ -21,6 +22,7 @@ function App() {
 
     return (
         <div className='text-default min-h-screen bg-white'>
+            <ToastContainer />
             {!isEducatorRoute && <Navbar/>}
             <Routes>
                 <Route path='/' element={<Home />} />
